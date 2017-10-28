2e4e6a0b9f686c505c81f06ca6c60e09b9efbd49
Ankit Arora <ankitarora@Ankits-MacBook-Pro.local>
2017-10-29 04:18:49 +0530

    Fixed refresh on each nested url

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('*', function (request, response){
+app.get('/*', function (request, response){
     response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
